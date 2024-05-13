d1e421e1bfd836389beee62e3ddbcc9119396a25
hasta.suci@gmail.com <hasta.suci@gmail.com>
2024-05-13 11:15:11 +0700

    read matrix script from external file.txt

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -4,17 +4,12 @@ print("Matrix script:")
 with open("encode.txt", "r") as file:
     lines = file.readlines()
     for line in lines:
-        print(line.strip()) 
+        print(line.strip())  
 
 rows, cols = map(int, lines[0].split())
-
 matrix = [list(line.strip()) for line in lines[1:]]
-matrix = [row + [' '] * (cols - len(row)) for row in matrix]
-
-transposed_matrix = list(zip(*matrix))
-transposed_text = "".join("".join(row) for row in transposed_matrix)
+transposed_matrix = ["".join(row) for row in zip(*matrix[:cols])]
+transposed_text = "".join(transposed_matrix)
 decoded_text = re.sub(r'[^a-zA-Z]', ' ', transposed_text)
 
 print("Decoded text:", decoded_text)
-
-
