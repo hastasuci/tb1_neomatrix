6635ded73b4e3f443006deeacbfa5de55cf98d10
hasta.suci@gmail.com <hasta.suci@gmail.com>
2024-05-14 22:42:29 +0700

    add function and condition

--- neomatrix.py
+++ neomatrix.py
@@ -1,20 +1,20 @@
 import re
 
-print("Matrix script:")
-with open("encode.txt", "r") as file:
-    lines = file.readlines()
-    for line in lines:
-        print(line.strip()) 
+def decode_matrix(file_name):
+    print("Matrix script:")
+    with open(file_name, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            print(line.strip())
 
-rows, cols = map(int, lines[0].split())
+    rows, cols = map(int, lines[0].split())
+    matrix = [list(line.strip()) for line in lines[1:]]
+    matrix = [row + [' '] * (cols - len(row)) for row in matrix]
 
-matrix = [list(line.strip()) for line in lines[1:]]
-matrix = [row + [' '] * (cols - len(row)) for row in matrix]
+    transposed_matrix = list(zip(*matrix))
+    transposed_text = "".join("".join(row) for row in transposed_matrix)
+    
+    decoded_text = re.sub(r'(?<=\w)([^\w\d]+)(?=\w)', ' ', transposed_text)
+    print("Decoded text:", decoded_text)
 
-transposed_matrix = list(zip(*matrix))
-transposed_text = "".join("".join(row) for row in transposed_matrix)
-decoded_text = re.sub(r'[^a-zA-Z]', ' ', transposed_text)
-
-print("Decoded text:", decoded_text)
-
-
+decode_matrix("encode.txt")
